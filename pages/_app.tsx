b649257dac6b6dc2219cec06716f0f9b53ae5779
Christian Alfoni <christianalfoni@gmail.com>
2021-07-30 10:45:09 +0200

    fullscreen

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,6 +12,11 @@ function MyApp({ Component, pageProps }: AppProps) {
           name="viewport"
           content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
         />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        />
         <meta name="description" content="Description" />
         <meta name="keywords" content="Keywords" />
         <title>Next.js PWA Example</title>
